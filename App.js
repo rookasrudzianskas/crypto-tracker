fdd8470e063ad8c3a55fd647f4a1f715360f826f
Rokas <rookas.rudzenskas@gmail.com>
2022-10-30 20:52:38 +0200

    Rokas

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {FontAwesome} from "@expo/vector-icons";
 
 export default function App() {
-    const price_change_percentage_24h = +0.1;
+    const price_change_percentage_24h = 0.1;
   return (
       <View className="flex-1 pt-16 bg-[#121212]">
         <TouchableOpacity className="flex-row items-center justify-between mx-4" activeOpacity={0.7}>
             <View className="flex-row items-center">
                 <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'}} className="w-10 h-10 object-contain" />
-                <View className="items-start ml-3 w-[200px]">
+                <View className="items-start ml-3 w-[200px] ">
                     <Text className="text-white text-xl font-semibold -mb-1">Bitcoin</Text>
                     <View className="flex-row items-center">
                         <View className="w-5 h-5 bg-gray-600 rounded-md items-center justify-center mr-2">
@@ -18,9 +18,9 @@ export default function App() {
                         <Text className="font-bold uppercase text-gray-400/90 mr-3 tracking-wider">BTC</Text>
                         <View className="flex-row items-center space-x-2">
                             {price_change_percentage_24h < 0  ? (
-                                <FontAwesome className="mt-1" style={{marginTop: 13}} name="sort-down" size={24} color="#FF4B4B" />
+                                <FontAwesome className="mt-1" style={{marginTop: 13}} name="sort-down" size={20} color="#FF4B4B" />
                             ) : (
-                                <FontAwesome className="mt-1" style={{marginTop: 13}} name="sort-up" size={24} color="#3DFF4F" />
+                                <FontAwesome className="mt-0" style={{marginTop: 13}} name="sort-up" size={20} color="#3DFF4F" />
                             )}
                             <Text className="font-bold uppercase text-gray-400/90 tracking-wide">1.02 %</Text>
                         </View>
