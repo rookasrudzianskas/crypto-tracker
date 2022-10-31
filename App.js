54f819a81b3337662826eeea13169d4cacda124c
Rokas <rookas.rudzenskas@gmail.com>
2022-10-31 18:07:21 +0200

    Rokas

--- App.js
+++ App.js
@@ -2,14 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { View} from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
 import CoinDetailedScreen from "./src/screens/CoinDetailedScreen";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
-      <View className="flex-1 bg-[#121212]">
-          {/*<HomeScreen />*/}
-          <CoinDetailedScreen />
-        <StatusBar style="light" />
-      </View>
+      <NavigationContainer
+          theme={{
+              colors: {
+                  background: "#121212",
+              },
+          }}
+      >
+          <View className="flex-1 bg-[#121212]">
+              {/*<HomeScreen />*/}
+              <CoinDetailedScreen />
+              <StatusBar style="light" />
+          </View>
+
+      </NavigationContainer>
   );
 }
 
---
+++ src/navigation/BottomTabNavigator.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import HomeScreen from "../screens/HomeScreen";
+
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: "grey",
+                tabBarStyle: {
+                    backgroundColor: "#181818",
+                },
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused, color }) => (
+                        <Entypo name="home" size={focused ? 30 : 25} color={color} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTabNavigator;
