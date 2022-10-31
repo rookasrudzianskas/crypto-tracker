4b7f83bed6973723f2a3f3a83e02000e3bf35ee4
Rokas <rookas.rudzenskas@gmail.com>
2022-10-31 14:02:17 +0200

    Rokas

--- src/screens/CoinDetailedScreen/components/CoinDetailedHeader/CoinDetailedHeader.js
+++ src/screens/CoinDetailedScreen/components/CoinDetailedHeader/CoinDetailedHeader.js
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import React, {useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import {FontAwesome, Ionicons} from "@expo/vector-icons";
+import {EvilIcons, FontAwesome, Ionicons} from "@expo/vector-icons";
 
 const CoinDetailedHeader = ({ coinId, name, usd, image, symbol, price_change_percentage_24h, market_cap_rank }) => {
     const times =  ['1h', '24h', '7d', '30d', '1y', 'All'];
@@ -9,15 +9,20 @@ const CoinDetailedHeader = ({ coinId, name, usd, image, symbol, price_change_per
 
     return (
         <View>
-            <View className="flex-row items-center space-x-2">
+            <View className="flex-row items-center">
+                <View className="flex-1 flex-row items-center space-x-2">
+                    <TouchableOpacity activeOpacity={0.7}>
+                        <Ionicons name="arrow-back-circle-outline" size={24} color="white" />
+                    </TouchableOpacity>
+                    <Text className="text-white text-lg font-semibold">{name || 'Loading...'}</Text>
+                </View>
                 <TouchableOpacity activeOpacity={0.7}>
-                    <Ionicons name="arrow-back-circle-outline" size={24} color="white" />
+                    <EvilIcons className="" name="user" size={27} color="white" />
                 </TouchableOpacity>
-                <Text className="text-white text-lg font-semibold">{name || 'Loading...'}</Text>
             </View>
             <View className="flex-row items-center justify-between">
-                <Text className="text-white text-2xl font-semibold">{(usd).toFixed(2) || 0} US$</Text>
-                <TouchableOpacity className={`flex-row items-center space-x-1 ${price_change_percentage_24h > 0 ? 'bg-[#3cbd48]' : 'bg-[#FF4B4B]'} px-3 py-2 rounded-md`} activeOpacity={0.7}>
+                <Text className="text-white text-2xl font-semibold tracking-wide">{(usd).toFixed(2) || 0} US$</Text>
+                <TouchableOpacity className={`flex-row items-center space-x-1 mt-2 ${price_change_percentage_24h > 0 ? 'bg-[#3cbd48]' : 'bg-[#FF4B4B]'} px-3 py-2 rounded-md`} activeOpacity={0.7}>
                     {price_change_percentage_24h > 0 ? (
                         <FontAwesome name="caret-up" style={{marginBottom: 2}} size={20} color="white" />
                     ) : (
