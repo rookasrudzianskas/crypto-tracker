66bd09c2c7be25c37712608d4fc89c774423bd27
Rokas <rookas.rudzenskas@gmail.com>
2022-11-01 17:36:17 +0200

    Rokas

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,36 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import cryptoData from "../../../assets/data/cryptocurrencies.json";
 import CoinItem from "../../components/CoinItem";
+import {getMarketData} from "../../services/requests";
 
 const HomeScreen = () => {
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchCoins = async (pageNumber) => {
+        if(loading) return;
+        setLoading(true);
+        const coinsData = await getMarketData(pageNumber);
+        setCoins(coinsData);
+        setLoading(false);
+    }
+
+    const refetchCoins = async () => {
+        if (loading) {
+            return;
+        }
+        setLoading(true);
+        const coinsData = await getMarketData()
+        setCoins(coinsData)
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchCoins();
+    }, []);
+
     return (
         <View className=" pt-16">
             <FlatList
