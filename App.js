3985995321cd2d606ca2d0d2366b5cfeab9d4d1f
Rokas <rookas.rudzenskas@gmail.com>
2022-11-01 19:17:59 +0200

    Rokas

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { View} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import Navigation from "./src/navigation";
+import WatchlistProvider from "./src/contexts/WatchlistContext";
 
 export default function App() {
   return (
@@ -12,11 +13,12 @@ export default function App() {
               },
           }}
       >
-          <View className="flex-1 bg-[#121212]">
-              <Navigation />
-              <StatusBar style="light" />
-          </View>
-
+          <WatchlistProvider>
+              <View className="flex-1 bg-[#121212]">
+                  <Navigation />
+                  <StatusBar style="light" />
+              </View>
+          </WatchlistProvider>
       </NavigationContainer>
   );
 }
