786c411cb6e29d3442e0ccebca15344ce7037cd5
Rokas <rookas.rudzenskas@gmail.com>
2022-11-02 12:58:43 +0200

    Rokas

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import CoinDetailedScreen from '../screens/CoinDetailedScreen';
 import BottomTabNavigator from './BottomTabNavigator';
+import AddNewAssetScreen from "../screens/AddNewAssetScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,21 @@ const Navigation = () => {
         >
             <Stack.Screen name="Root" component={BottomTabNavigator} />
             <Stack.Screen name="CoinDetailedScreen" component={CoinDetailedScreen} />
+            <Stack.Screen
+                name="AddNewAssetScreen"
+                component={AddNewAssetScreen}
+                options={{
+                    headerShown: true,
+                    title: "Add New Asset",
+                    headerStyle: {
+                        backgroundColor: "#121212",
+                    },
+                    headerTintColor: "white",
+                    headerTitleStyle: {
+                        fontWeight: "bold",
+                    },
+                }}
+            />
         </Stack.Navigator>
     )
 }
