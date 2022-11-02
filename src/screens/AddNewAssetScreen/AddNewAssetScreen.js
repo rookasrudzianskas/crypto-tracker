d820a0f32ff66efd2234a8b18f5731e72824466b
Rokas <rookas.rudzenskas@gmail.com>
2022-11-02 13:17:48 +0200

    Rokas

--- src/screens/AddNewAssetScreen/AddNewAssetScreen.js
+++ src/screens/AddNewAssetScreen/AddNewAssetScreen.js
@@ -1,14 +1,16 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, TextInput} from 'react-native';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 const AddNewAssetScreen = () => {
+    const navigation = useNavigation();
     return (
         <View className="mx-4">
             <View className="border border-gray-500 border-[2px] rounded py-2 px-2 bg-gray-800/30 mt-7">
                 <TextInput autoCapitalize={'none'} className="text-gray-300 bg-transparent" placeholder="Asset Name" />
             </View>
-            <View className="h-screen items-center">
+            <View className="h-[90%] items-center">
                 <View className="mt-16">
                     <View className="flex-row items-center justify-center">
                         <TextInput keyboardType={'numeric'} className="text-gray-800 text-[90px]" placeholder={'0'}/>
@@ -18,6 +20,11 @@ const AddNewAssetScreen = () => {
                         <Text className="text-gray-500 font-bold tracking-wider">47018.00$ per coin</Text>
                     </View>
                 </View>
+                <TouchableOpacity activeOpacity={0.7} className="absolute bottom-6 w-full mx-4 flex-row items-center justify-center py-3 bg-blue-600 rounded-md"
+                                  onPress={() => navigation.navigate("AddNewAssetScreen")}
+                >
+                    <Text className="text-white font-bold">Add New Asset</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
