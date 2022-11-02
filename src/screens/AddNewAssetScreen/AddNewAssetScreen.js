dd116ae828380a4c671782c9ace623b9e073f60d
Rokas <rookas.rudzenskas@gmail.com>
2022-11-02 13:09:06 +0200

    Rokas

--- src/screens/AddNewAssetScreen/AddNewAssetScreen.js
+++ src/screens/AddNewAssetScreen/AddNewAssetScreen.js
@@ -1,13 +1,24 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
 
 const AddNewAssetScreen = () => {
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+        <View className="mx-4">
+            <View className="border border-gray-500 border-[2px] rounded py-2 px-2 bg-gray-800/30 mt-7">
+                <TextInput autoCapitalize={'none'} className="text-gray-300 bg-transparent" placeholder="Asset Name" />
+            </View>
+            <View className="h-screen items-center">
+                <View className="mt-16">
+                    <View className="flex-row items-center justify-center">
+                        <Text className="text-gray-800 text-[90px]">0</Text>
+                        <Text className="text-gray-400 font-bold uppercase tracking-wider -mt-12 ml-3 text-[17px]">BTC</Text>
+                    </View>
+                    <View className="items-center justify-center">
+                        <Text className="text-gray-500 font-bold tracking-wider">47018.00$ per coin</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 };
