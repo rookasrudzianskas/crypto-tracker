4e7836c6ea6ee3f0b803d6500f733a2825e66ddc
Rokas <rookas.rudzenskas@gmail.com>
2022-11-02 15:36:14 +0200

    Rokas

--- src/components/CoinItem/CoinItem.js
+++ src/components/CoinItem/CoinItem.js
@@ -29,31 +29,34 @@ const CoinItem = ({marketCoin, index}) => {
     }
 
     return (
-        <TouchableOpacity onPress={() => navigation.navigate('CoinDetailedScreen', { coinId: id})} className="flex-row items-center justify-between mx-4 my-2" activeOpacity={0.7}>
-            <View className="flex-row items-center">
-                <Image source={{ uri: image || 'https://as2.ftcdn.net/v2/jpg/02/88/85/71/1000_F_288857162_l7ZOOsEveQf1d8PMsNC6HMQFeqafLJhx.jpg'}} className="w-10 h-10 object-contain" />
-                <View className="items-start ml-3 w-[200px] ">
-                    <Text className="text-white text-xl font-semibold -mb-1">{name || 'Loading...'}</Text>
-                    <View className="flex-row items-center">
-                        <View className="px-[4px] py-[2px] bg-gray-600 rounded-md items-center justify-center mr-2 mt-1">
-                            <Text className="text-white font-bold">{index}</Text>
-                        </View>
-                        <Text className="font-bold uppercase text-gray-400/90 mr-2 tracking-wider mt-1">{symbol || 'Loading...'}</Text>
-                        <View className="flex-row items-center space-x-2 mt-1">
-                            {price_change_percentage_24h && price_change_percentage_24h > 0  ? (
-                                <FontAwesome name="caret-up" size={20} color="#3DFF4F" />
-                            ) : (
-                                <FontAwesome name="caret-down" size={20} color="#FF4B4B" />
-                            )}
-                            <Text className="font-bold uppercase text-gray-400/90 tracking-wide">{price_change_percentage_24h?.toFixed(2) || 0} %</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('CoinDetailedScreen', { coinId: id})} className="mx-4 my-2" activeOpacity={0.7}>
+            <View className="flex-row items-center justify-between">
+                <View className="flex-row items-center">
+                    <Image source={{ uri: image || 'https://as2.ftcdn.net/v2/jpg/02/88/85/71/1000_F_288857162_l7ZOOsEveQf1d8PMsNC6HMQFeqafLJhx.jpg'}} className="w-10 h-10 object-contain" />
+                    <View className="items-start ml-3 w-[200px] ">
+                        <Text className="text-white text-xl font-semibold -mb-1">{name || 'Loading...'}</Text>
+                        <View className="flex-row items-center">
+                            <View className="px-[4px] py-[2px] bg-gray-600 rounded-md items-center justify-center mr-2 mt-1">
+                                <Text className="text-white font-bold">{index}</Text>
+                            </View>
+                            <Text className="font-bold uppercase text-gray-400/90 mr-2 tracking-wider mt-1">{symbol || 'Loading...'}</Text>
+                            <View className="flex-row items-center space-x-2 mt-1">
+                                {price_change_percentage_24h && price_change_percentage_24h > 0  ? (
+                                    <FontAwesome name="caret-up" size={20} color="#3DFF4F" />
+                                ) : (
+                                    <FontAwesome name="caret-down" size={20} color="#FF4B4B" />
+                                )}
+                                <Text className="font-bold uppercase text-gray-400/90 tracking-wide">{price_change_percentage_24h?.toFixed(2) || 0} %</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
+                <View className="">
+                    <Text className="text-xl font-bold text-white text-right">{current_price?.toFixed(2) || 0}</Text>
+                    <Text className="font-bold text-gray-400/90 text-[15px] mt-1">MCap {normalizeMarketCap(market_cap)} T</Text>
+                </View>
             </View>
-            <View className="">
-                <Text className="text-xl font-bold text-white text-right">{current_price?.toFixed(2) || 0}</Text>
-                <Text className="font-bold text-gray-400/90 text-[15px] mt-1">MCap {normalizeMarketCap(market_cap)} T</Text>
-            </View>
+            <View className="border-b border-gray-900 mt-2"/>
         </TouchableOpacity>
     );
 };
